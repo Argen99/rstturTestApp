f57b0374ab12d3cdaa64198bb86c46a81d45054e
Argen99 <argen1999@mail.ru>
2023-11-02 21:25:56 +0600

    Initial commit

---
+++ app/src/main/java/com/example/rstturtestapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.rstturtestapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.rstturtestapp.ui.theme.RstturTestAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RstturTestAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    
+                }
+            }
+        }
+    }
+}
